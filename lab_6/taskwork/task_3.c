a8f80902f018a002607967967f56ea2261481290
rudnmk <1132226149@pfur.ru>
2023-04-16 18:31:47 +0300

    //header исправил задание №3

--- lab_6/taskwork/task_3.c
+++ lab_6/taskwork/task_3.c
@@ -6,8 +6,8 @@ int s;
 char tmp;
 int pos;
 void print(char *array);
-void append(char *array, char tmp);
-void insert(char *array, char tmp, int pos);
+void append(char *array, char *tmp);
+void insert(char *array, char *tmp, int pos);
 
 
 void main() {
@@ -48,26 +48,26 @@ void main() {
 
 void print(char *array) {
     for (int m = 0; m < s; m++) {
-        printf("arr[%i]: %c \n", m, *(array + m));
+        printf("arr[%i]: %c \n", (m + 1), *(array + m));
     }
 }
 
-void append(char *array, char tmp) {
+void append(char *array, char *tmp) {
     s++;
-    *(array + s - 1) = tmp;
+    *(array + s - 1) = *tmp;
 
 }
 
-void insert(char *array, char tmp, int pos) {
-    s++;
-    if (pos < 0 || pos > s) {
-        printf("Невозможно выполнить операцию: задания позиция выходит за пределы массива!");
+void insert(char *array, char *tmp, int pos) {
+    if (pos < 0 || pos > s + 1) {
+        printf("Невозможно выполнить операцию: задания позиция выходит за пределы массива!\n");
     }
 
     else {
+        s++;
         for (int m = 1; m <= s; m++) {
             if ((s - m) == (pos - 1)) {
-                *(array + s - m) = tmp;
+                *(array + s - m) = *tmp;
                 break;
             }
             else {
