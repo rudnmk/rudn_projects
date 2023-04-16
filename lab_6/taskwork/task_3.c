702c68b21240df51beff5031438b2b9a2617a3c9
rudnmk <1132226149@pfur.ru>
2023-04-16 18:26:06 +0300

    //header выполнил задание из лабораторной
    //body выполнил задание №3

---
+++ lab_6/taskwork/task_3.c
@@ -0,0 +1,78 @@
+//динамический массив
+
+#include <stdio.h>
+
+int s;
+char tmp;
+int pos;
+void print(char *array);
+void append(char *array, char tmp);
+void insert(char *array, char tmp, int pos);
+
+
+void main() {
+    printf("Сколько будет элементов в массиве?: ");
+    scanf("%i", &s);
+    char arr[s];
+    for (int m = 0; m < s; m++) {
+        printf("%c", arr);
+        printf("Введите символ: ");
+        scanf(" %c", &tmp);
+        arr[m] = tmp;
+    }
+    //проверка работы функции append:
+    append(&arr, "t");
+    print(&arr);
+    append(&arr, " ");
+    print(&arr);
+    append(&arr, "3");
+    print(&arr);
+    append(&arr, "z");
+    print(&arr);
+    append(&arr, "r");
+    print(&arr);
+
+    //проверка работы функции insert:
+    insert(&arr, "t", 3);
+    print(&arr);
+    insert(&arr, "4", 5);
+    print(&arr);
+    insert(&arr, "e", 1);
+    print(&arr);
+    //проверка работы функции insert при некорректных вводах pos(pos < 0 или pos > size):
+    insert(&arr, "t", 99);
+    print(&arr);
+    insert(&arr, "t", -11);
+    print(&arr);
+}
+
+void print(char *array) {
+    for (int m = 0; m < s; m++) {
+        printf("arr[%i]: %c \n", m, *(array + m));
+    }
+}
+
+void append(char *array, char tmp) {
+    s++;
+    *(array + s - 1) = tmp;
+
+}
+
+void insert(char *array, char tmp, int pos) {
+    s++;
+    if (pos < 0 || pos > s) {
+        printf("Невозможно выполнить операцию: задания позиция выходит за пределы массива!");
+    }
+
+    else {
+        for (int m = 1; m <= s; m++) {
+            if ((s - m) == (pos - 1)) {
+                *(array + s - m) = tmp;
+                break;
+            }
+            else {
+                *(array + s - m) = *(array + s - m - 1);
+            }
+        }
+    }
+}
